4034560aa00a7140891d8c31302e107427513df2
zhaogaoxing <zhaogaoxing@baijiashilian.com>
2024-08-07 09:54:55 +0800

    fix: float_dsp simd 修复

--- libavutil/webassembly/float_dsp_init.c
+++ libavutil/webassembly/float_dsp_init.c
@@ -25,7 +25,9 @@
 #include <stdio.h>
 
 static void ff_vector_fmul_webassembly(float *dst, const float *src0, const float *src1, int len) {
-    for (int i = 0; i < len; i += 4) {
+    int length = (len >> 2) << 2;
+    int i;
+    for (i = 0; i < length; i += 4) {
         wasm_v128_store(
             &dst[i],
             wasm_f32x4_mul(
@@ -33,11 +35,16 @@ static void ff_vector_fmul_webassembly(float *dst, const float *src0, const floa
                 wasm_v128_load(&src1[i])
             )
         );
+    }
+    for (i = length; i < len; i++) {
+        dst[i] = src0[i] * src1[i];
     }
 }
 
 static void ff_vector_dmul_webassembly(double *dst, const double *src0, const double *src1, int len) {
-    for (int i = 0; i < len; i += 2) {
+    int length = (len >> 1) << 1;
+    int i;
+    for (i = 0; i < length; i += 2) {
         wasm_v128_store(
             &dst[i],
             wasm_f64x2_mul(
@@ -46,13 +53,17 @@ static void ff_vector_dmul_webassembly(double *dst, const double *src0, const do
             )
         );
     }
+    for (i = length; i < len; i++) {
+        dst[i] = src0[i] * src1[i];
+    }
 }
 
 static void ff_vector_fmac_scalar_webassembly(float *dst, const float *src, float mul, int len) {
-
+    int length = (len >> 2) << 2;
+    int i;
     v128_t v128mul = wasm_f32x4_splat(mul);
 
-    for (int i = 0; i < len; i += 4) {
+    for (i = 0; i < length; i += 4) {
         wasm_v128_store(
             &dst[i],
             wasm_f32x4_mul(
@@ -60,13 +71,18 @@ static void ff_vector_fmac_scalar_webassembly(float *dst, const float *src, floa
                 v128mul
             )
         );
+    }
+    for (i = length; i < len; i++) {
+        dst[i] += src[i] * mul;
     }
 }
 
 static void ff_vector_fmul_scalar_webassembly(float *dst, const float *src, float mul, int len) {
+    int length = (len >> 2) << 2;
+    int i;
     v128_t v128mul = wasm_f32x4_splat(mul);
 
-    for (int i = 0; i < len; i += 4) {
+    for (i = 0; i < length; i += 4) {
         wasm_v128_store(
             &dst[i],
             wasm_f32x4_mul(
@@ -75,12 +91,18 @@ static void ff_vector_fmul_scalar_webassembly(float *dst, const float *src, floa
             )
         );
     }
+
+    for (i = length; i < len; i++) {
+        dst[i] = src[i] * mul;
+    }
 }
 
 static void ff_vector_dmac_scalar_webassembly(double *dst, const double *src, double mul, int len) {
+    int length = (len >> 1) << 1;
+    int i;
     v128_t v128mul = wasm_f64x2_splat(mul);
 
-    for (int i = 0; i < len; i += 2) {
+    for (i = 0; i < length; i += 2) {
         wasm_v128_store(
             &dst[i],
             wasm_f64x2_mul(
@@ -89,14 +111,39 @@ static void ff_vector_dmac_scalar_webassembly(double *dst, const double *src, do
             )
         );
     }
+    for (i = length; i < len; i++) {
+        dst[i] += src[i] * mul;
+    }
 }
+
+static void ff_vector_dmul_scalar_webassembly(double *dst, const double *src, double mul, int len) {
+    int length = (len >> 1) << 1;
+    int i;
+    v128_t v128mul = wasm_f64x2_splat(mul);
+
+    for (i = 0; i < length; i += 2) {
+        wasm_v128_store(
+            &dst[i],
+            wasm_f64x2_mul(
+                wasm_v128_load(&src[i]),
+                v128mul
+            )
+        );
+    }
+    for (i = length; i < len; i++) {
+        dst[i] = src[i] * mul;
+    }
+}
+
 
 static void ff_vector_fmul_window_webassembly(float *dst, const float *src0, const float *src1, const float *win, int len) {
     
 }
 
 static void ff_vector_fmul_add_webassembly(float *dst, const float *src0, const float *src1, const float *src2, int len) {
-    for (int i = 0; i < len; i += 4) {
+    int length = (len >> 2) << 2;
+    int i;
+    for (i = 0; i < length; i += 4) {
         wasm_v128_store(
             &dst[i],
             wasm_f32x4_add(
@@ -108,6 +155,9 @@ static void ff_vector_fmul_add_webassembly(float *dst, const float *src0, const 
             ) 
         );
     }
+    for (i = length; i < len; i++) {
+        dst[i] = src0[i] * src1[i] + src2[i];
+    }
 }
 
 static void ff_vector_fmul_reverse_webassembly(float *dst, const float *src0, int len) {
@@ -115,13 +165,12 @@ static void ff_vector_fmul_reverse_webassembly(float *dst, const float *src0, in
 }
 
 static float ff_scalarproduct_float_webassembly(const float *v1, const float *v2, int len) {
-    float p = 0.0;
+    int length = (len >> 2) << 2;
     int i;
-    int length = len >> 2 << 2;
     v128_t sum = wasm_f32x4_splat(0.0);
 
     for (i = 0; i < length; i += 4) {
-        wasm_f32x4_add(
+        sum = wasm_f32x4_add(
             sum,
             wasm_f32x4_mul(
                 wasm_v128_load(&v1[i]),
@@ -129,10 +178,7 @@ static float ff_scalarproduct_float_webassembly(const float *v1, const float *v2
             )
         );
     }
-    float* pp = &sum;
-    for (i = 0; i < 4; i++) {
-        p += *(pp + i);
-    }
+    float p = ((__f32x4)sum)[0] + ((__f32x4)sum)[1] + ((__f32x4)sum)[2] + ((__f32x4)sum)[3];
 
     for (i = length; i < len; i++) {
         p += v1[i] * v2[i];
@@ -142,15 +188,22 @@ static float ff_scalarproduct_float_webassembly(const float *v1, const float *v2
 }
 
 static void ff_butterflies_float_webassembly(float* src0, float* src1, int len) {
+    int length = (len >> 2) << 2;
     int i;
 
-    for (i = 0; i < len; i += 4) {
+    for (i = 0; i < length; i += 4) {
         v128_t t = wasm_f32x4_sub(wasm_v128_load(&src0[i]), wasm_v128_load(&src1[i]));
         wasm_v128_store(
             &src0[i],
             wasm_f32x4_add(wasm_v128_load(&src0[i]), wasm_v128_load(&src1[i]))
         );
         wasm_v128_store(&src1[i], t);
+    }
+
+    for (i = length; i < len; i++) {
+        float t = src0[i] - src1[i];
+        src0[i] += src1[i];
+        src1[i] = t;
     }
 }
 
@@ -163,6 +216,7 @@ void ff_float_dsp_init_webassembly(AVFloatDSPContext *fdsp)
     fdsp->vector_fmac_scalar = ff_vector_fmac_scalar_webassembly;
     fdsp->vector_fmul_scalar = ff_vector_fmul_scalar_webassembly;
     fdsp->vector_dmac_scalar = ff_vector_dmac_scalar_webassembly;
+    fdsp->vector_dmul_scalar = ff_vector_dmul_scalar_webassembly;
     fdsp->vector_fmul_add = ff_vector_fmul_add_webassembly;
     fdsp->scalarproduct_float = ff_scalarproduct_float_webassembly;
     fdsp->butterflies_float = ff_butterflies_float_webassembly;
